a28685aab4e0e4e0a7db164a77714ed00fce47b1
= <obabou50@gmail.com>
2021-09-16 07:33:49 -0700

    evening commit

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,19 @@
-import { Facebook, Instagram, Pinterest, Twitter } from "@material-ui/icons";
+import {
+  Facebook,
+  Instagram,
+  MailOutline,
+  Phone,
+  Pinterest,
+  Room,
+  Twitter,
+} from "@material-ui/icons";
 import styled from "styled-components";
+// import { mobile } from "../responsive";
 
 const Container = styled.div`
   display: flex;
 `;
+
 const Left = styled.div`
   flex: 1;
   display: flex;
@@ -31,7 +41,6 @@ const SocialIcon = styled.div`
   align-items: center;
   justify-content: center;
   margin-right: 20px;
-  margin-top: 10px;
 `;
 
 const Center = styled.div`
@@ -39,41 +48,92 @@ const Center = styled.div`
   padding: 20px;
 `;
 
-const Title = styled.h1``;
+const Title = styled.h3`
+  margin-bottom: 30px;
+`;
+
+const List = styled.ul`
+  margin: 0;
+  padding: 0;
+  list-style: none;
+  display: flex;
+  flex-wrap: wrap;
+`;
+
+const ListItem = styled.li`
+  width: 50%;
+  margin-bottom: 10px;
+`;
 
 const Right = styled.div`
   flex: 1;
   padding: 20px;
 `;
 
+const ContactItem = styled.div`
+  margin-bottom: 20px;
+  display: flex;
+  align-items: center;
+`;
+
+const Payment = styled.img`
+  width: 50%;
+`;
+
 const Footer = () => {
   return (
     <Container>
       <Left>
-        <Logo>Omar.</Logo>
+        <Logo>OMAR.</Logo>
         <Desc>
-          {" "}
           There are many variations of passages of Lorem Ipsum available, but
           the majority have suffered alteration in some form, by injected
           humour, or randomised words which don’t look even slightly believable.
         </Desc>
+        <SocialContainer>
+          <SocialIcon color="3B5999">
+            <Facebook />
+          </SocialIcon>
+          <SocialIcon color="E4405F">
+            <Instagram />
+          </SocialIcon>
+          <SocialIcon color="55ACEE">
+            <Twitter />
+          </SocialIcon>
+          <SocialIcon color="E60023">
+            <Pinterest />
+          </SocialIcon>
+        </SocialContainer>
       </Left>
-      <SocialContainer>
-        <SocialIcon color="3B5999">
-          <Facebook />
-        </SocialIcon>
-        <SocialIcon color="E4405F">
-          <Instagram />
-        </SocialIcon>
-        <SocialIcon color="55ACEE">
-          <Twitter />
-        </SocialIcon>
-        <SocialIcon color="E60023">
-          <Pinterest />
-        </SocialIcon>
-      </SocialContainer>
-      <Center></Center>
-      <Right></Right>
+      <Center>
+        <Title>Useful Links</Title>
+        <List>
+          <ListItem>Home</ListItem>
+          <ListItem>Cart</ListItem>
+          <ListItem>Man Fashion</ListItem>
+          <ListItem>Woman Fashion</ListItem>
+          <ListItem>Accessories</ListItem>
+          <ListItem>My Account</ListItem>
+          <ListItem>Order Tracking</ListItem>
+          <ListItem>Wishlist</ListItem>
+          <ListItem>Wishlist</ListItem>
+          <ListItem>Terms</ListItem>
+        </List>
+      </Center>
+      <Right>
+        <Title>Contact</Title>
+        <ContactItem>
+          <Room style={{ marginRight: "10px" }} /> 407 st Niboye Kicukiro,
+          Kigali City Tobinchester 98336
+        </ContactItem>
+        <ContactItem>
+          <Phone style={{ marginRight: "10px" }} /> +250 78 99 19 478
+        </ContactItem>
+        <ContactItem>
+          <MailOutline style={{ marginRight: "10px" }} /> obabou50@gmail.com
+        </ContactItem>
+        <Payment src="https://i.ibb.co/Qfvn4z6/payment.png" />
+      </Right>
     </Container>
   );
 };
