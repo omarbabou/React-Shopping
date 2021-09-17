a9b505d6887cbb51a567e5875c29b7be0993e052
= <obabou50@gmail.com>
2021-09-17 01:50:42 -0700

    morining commit

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -27,10 +27,15 @@ const Title = styled.h1`
 
 const Form = styled.form`
   display: flex;
-  fle-wrap: wrap;
+  flex-wrap: wrap;
 `;
 
-const Input = styled.input``;
+const Input = styled.input`
+  flex: 1;
+  min-width: 40%;
+  margin: 20px 10px 0px 0px;
+  padding: 10px;
+`;
 
 const Agreement = styled.span``;
 
