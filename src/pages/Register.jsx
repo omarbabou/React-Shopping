20ca2383807cd6958bf8b3bb0337028dacbaf45a
= <obabou50@gmail.com>
2021-09-17 02:58:42 -0700

    4th commit

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -9,6 +9,7 @@ const Container = styled.div`
     ),
     url("https://images.pexels.com/photos/6984661/pexels-photo-6984661.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940")
       center;
+  background-size: cover;
   display: flex;
   align-items: center;
   justify-content: center;
@@ -37,9 +38,21 @@ const Input = styled.input`
   padding: 10px;
 `;
 
-const Agreement = styled.span``;
+const Agreement = styled.span`
+  font-size: 12px;
+  margin: 20px 0px;
+`;
 
-const Button = styled.button``;
+const Button = styled.button`
+  width: 40%;
+  border: none;
+  padding: 15px 20px;
+  background-color: teal;
+  color: white;
+  cursor: pointer;
+  flex: 6;
+  font-size: 15px;
+`;
 
 const Register = () => {
   return (
